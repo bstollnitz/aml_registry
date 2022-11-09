0f2dc3f311ae93f2656332f2c0f7e1fbc3bd04c2
Bea Stollnitz <bstollnitz@microsoft.com>
2022-11-09 14:27:16 -0800

    Fixed directory name in code.

--- aml_registry/test_data/generate_images.py
+++ aml_registry/test_data/generate_images.py
@@ -9,9 +9,9 @@ import pandas
 from PIL import Image
 from torchvision import datasets
 
-DATA_DIR = "aml_pipeline_cli/test_data/data"
-IMAGES_DIR = "aml_pipeline_cli/test_data/images"
-TEST_DATA_DIR = "aml_pipeline_cli/test_data"
+DATA_DIR = "aml_registry/test_data/data"
+IMAGES_DIR = "aml_registry/test_data/images"
+TEST_DATA_DIR = "aml_registry/test_data"
 
 
 def generate_images(num_images: int) -> None:
